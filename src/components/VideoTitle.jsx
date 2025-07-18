43814c9a937a344268b8da7060904bec784ee1a0
vedant_dagade <vedantdagade21@gmail.com>
2025-07-18 14:06:36 +0530

    api work completed for trailer started background trailer

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -6,10 +6,10 @@ import { ArrowRight, ArrowLeft , Play , Info } from "lucide-react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="pt-36 px-12 ">
-      <h1 className="font-bold text-6xl">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <h1 className="font-bold text-6xl w-1/2">{title}</h1>
+      <p className="py-6 text-lg ">{overview}</p>
       <div className="">
-        <Button className="mx-4 text-lg " variant="secondary"> <Play /> Play</Button>
+        <Button className="mx-4 text-lg " variant="default"><Play/> Play</Button>
         <Button className="text-lg " variant="destructive"><Info /> More Info</Button>
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // 👈 make sure to import path
+import path from "path";
 import { fileURLToPath } from "url";
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    host: "localhost", // or "0.0.0.0" if you're on WSL or testing on mobile
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: "ws",
+      host: "localhost", // or your LAN IP if accessing from another device
+      port: 5173,
     },
   },
 });
