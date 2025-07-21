a081cd90afa261e694c2ddfb34388d40b4d51db7
vedant_dagade <vedantdagade21@gmail.com>
2025-07-22 02:07:33 +0530

    start the moviesuggestion page ui

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,14 +1,29 @@
 import { Button } from "./ui/button";
 import GptSearch from "./GptSearch";
 import lang from "@/utils/languageConstant";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useRef } from "react";
 import openai from "@/utils/openai";
+import { API_OPTIONS } from "@/utils/constant";
+import { addGptMovieResult } from "@/utils/gptSlice";
 
 const GptSearchBar = () => {
   const searchText = useRef(null);
+  const dispatch = useDispatch();
 
   const langKey = useSelector((Store) => Store.config.lang);
+
+
+  //take moive and search tmdb
+  const searchMovieTMDB = async(movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" + movie +"&include_adult=false&language=en-US&page=1", API_OPTIONS
+    );
+
+    const json = await data.json();
+
+    return json.results;
+  } 
 
   const handleGptSearchClick = async () => {
     //make an api call to GPT Api and get Movie Results.
@@ -20,7 +35,23 @@ const GptSearchBar = () => {
       messages: [{ role: "user", content: gptQuery}],
     });
 
-    console.log(gptResults.choices);
+    if (!gptResults.choices) {
+      alert("Something went wrong with GPT. Try again later.");
+      return;
+    }
+
+    //give array of movies
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+
+    //For each movie I will search TMDB API.
+
+    const promiseArray = gptMovies.map(movie => searchMovieTMDB(movie));
+
+    const tmdbResults = await Promise.all(promiseArray);
+
+    console.log(tmdbResults);
+
+    dispatch(addGptMovieResult({movieNames: gptMovies,movieResults: tmdbResults}));
   };
 
   return (
--- src/utils/gptSlice.jsx
+++ src/utils/gptSlice.jsx
@@ -4,6 +4,7 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     showGptSearch: false,
+    gptMovies: null,
   },
 
   reducers: {
@@ -13,10 +14,16 @@ const gptSlice = createSlice({
     setGptSearch: (state, action) => {
       state.showGptSearch = action.payload;
     },
+    addGptMovieResult: (state , action) => {
+      const {movieNames , movieResults} = action.payload;
+      state.movieNames = movieNames;
+      state.movieResults = movieResults;
+
+    }
   },
 });
 
 
-export const { toggleGptSearchView , setGptSearch} = gptSlice.actions;
+export const { toggleGptSearchView , setGptSearch , addGptMovieResult} = gptSlice.actions;
 
 export default gptSlice.reducer;
